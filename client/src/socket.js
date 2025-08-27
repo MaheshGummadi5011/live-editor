ef5ee1f7fd24362ee9d52152a940423869165b17
Mahesh Gummadi <maheshgummadi5011@gmail.com>
2025-08-27 14:19:41 +0530

    Updated project with new changes

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,8 @@
 // client/src/socket.js
 import { io } from 'socket.io-client';
 
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'https://live-editor-qmo9.onrender.com';
+
 export const initSocket = async () => {
     const options = {
         'force new connection': true,
@@ -8,5 +10,5 @@ export const initSocket = async () => {
         timeout: 10000,
         transports: ['websocket'],
     };
-    return io('http://localhost:4000', options);
+    return io(BACKEND_URL, options);
 };
